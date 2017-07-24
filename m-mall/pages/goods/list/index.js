8e38deb00a0f93bc1e0b3b92939fec534cd0c9b1
cmh2016 <15675523332@163.com>
2017-07-24 18:20:24 +0800

    商品list修改

--- m-mall/pages/goods/list/index.js
+++ m-mall/pages/goods/list/index.js
@@ -9,14 +9,14 @@ Page({
             hidden: !0,
             icon: '../../../assets/images/iconfont-empty.png',
         },
+        show:true
     },
     onLoad(option) {
-        this.goods = App.HttpResource('/goods/:id', {id: '@id'})
         this.setData({
             type: option.type, 
             keyword: option.keyword && decodeURI(option.keyword), 
         })
-        this.onPullDownRefresh()
+        this.getList();
     },
     onReady() {
         const keyword = this.data.keyword
@@ -25,58 +25,88 @@ Page({
             title: `搜索商品：${keyword}`, 
         })
     },
-    initData() {
-        const type = this.data.type
-        const keyword = this.data.keyword
-
-        this.setData({
-            goods: {
-                items: [],
-                params: {
-                    page : 1,
-                    limit: 10,
-                    type : type,
-                    keyword : keyword,
-                },
-                paginate: {}
-            }
-        })
-    },
-    navigateTo(e) {
-        console.log(e)
-        App.WxService.navigateTo('/pages/goods/detail/index', {
-            id: e.currentTarget.dataset.id
-        })
-    },
     getList() {
-        const goods = this.data.goods
-        const params = goods.params
-
-        // App.HttpService.getGoods(params)
-        this.goods.queryAsync(params)
-        .then(data => {
-            console.log(data)
-            if (data.meta.code == 0) {
-                data.data.items.forEach(n => n.thumb_url = App.renderImage(n.images[0] && n.images[0].path))
-                goods.items = [...goods.items, ...data.data.items]
-                goods.paginate = data.data.paginate
-                goods.params.page = data.data.paginate.next
-                goods.params.limit = data.data.paginate.perPage
-                this.setData({
-                    goods: goods,
-                    'prompt.hidden': goods.items.length,
-                })
-            }
+      var uid = wx.getStorageSync('uid');
+      var token = wx.getStorageSync('token');
+      var that = this;
+      if (wx.showLoading) {
+        wx.showLoading({
+          title: "加载中",
+          mask: true
         })
+      } else {
+        // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+        wx.showModal({
+          title: '提示',
+          content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+        })
+      }
+      wx.request({
+        url: App.api + '/item/list', //仅为示例，并非真实的接口地址
+        data: {
+          uid: uid,
+          token: token
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          //关闭loading
+          if (wx.hideLoading) {
+            wx.hideLoading()
+          } else {
+            // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+            wx.showModal({
+              title: '提示',
+              content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+            })
+          }
+          if (res.data.code == 0) {
+            that.setData({
+              goods: res.data.data
+            })
+            if (res.data.data.spec_list.length == 0) {
+              //设置规格数据空提示
+              let prompts = that.data.prompt;
+              prompts.hidden = false;
+              that.setData({
+                prompt: prompts
+              })
+            }
+            if (res.data.data.attr_list.length == 0) {
+              //设置属性数据空提示
+              let prompts = that.data.prompt2;
+              prompts.hidden = false;
+              that.setData({
+                prompt2: prompts
+              })
+            }
+            if (res.data.data.content.length == 0) {
+              //设置详情数据空提示
+              let prompts = that.data.prompt3;
+              prompts.hidden = false;
+              that.setData({
+                prompt3: prompts
+              })
+            }
+            var article = res.data.data.content;
+            WxParse.wxParse('article', 'html', article, that, 5);
+          } else if (res.data.code == -1) {
+            App.error(res.data.msg)
+
+          }
+        }
+      })
     },
-    onPullDownRefresh() {
-        console.info('onPullDownRefresh')
-        this.initData()
-        this.getList()
+    //切换显示
+    show(){
+      this.setData({
+        show: false
+      })
     },
-    onReachBottom() {
-        console.info('onReachBottom')
-        if (!this.data.goods.paginate.hasNext) return
-        this.getList()
-    },
+    hide() {
+      this.setData({
+        show: true
+      })
+    }
 })

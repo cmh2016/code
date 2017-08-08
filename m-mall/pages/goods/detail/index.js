2ef73cb14f623f3cd80d38750507fca8ddd7adc6
cmh2016 <15675523332@163.com>
2017-08-09 00:27:43 +0800

    商品详情完善dialog功能及图片联动和价格联动

--- m-mall/pages/goods/detail/index.js
+++ m-mall/pages/goods/detail/index.js
@@ -1,7 +1,8 @@
 const App = getApp()
 var WxParse = require('../../../wxParse/wxParse.js');
+var Zan = require('../../../zanui-weapp/dist/index');
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
-Page({
+Page(Object.assign({}, Zan.Quantity, {
     data: {
         indicatorDots: !0,
         vertical: !1,
@@ -10,7 +11,7 @@ Page({
         duration: 1000,
         current: 0,
         goods: {},
-        tabs: ["商品详情", "商品属性", "商品规格"],
+        tabs: ["商品详情", "商品属性"],
         activeIndex:0,
         sliderOffset: 0,
         sliderLeft: 0,
@@ -23,7 +24,58 @@ Page({
         prompt3: {
             hidden: true,
         },
-        showDialog: false
+        showDialog: false,
+        spec_list_color:[],
+        spec_list_chima:[],
+        spec_price_list:[],
+        quantity1: {
+            quantity: 1,
+            min: 1
+        },
+        color_label_active:-1,
+        color_label_active2:-1,
+        dialogTopText:"请选择颜色和规格",
+        chioceColorText:'',
+        chioceGuigeText:'',
+        zuhePrice: '',
+    },
+    //选择颜色
+    chioceColor(e){
+        var that = this;
+        var index = e.currentTarget.dataset.index;
+        var id = e.currentTarget.dataset.item_id;
+        that.setData({
+            color_label_active: index,
+            chioceColorText:id,
+            zuhePrice: id + "_" + that.data.chioceGuigeText,
+            dialog_image: that.data.spec_list_color[index].src,
+            dialogTopText: "已选择：" + that.data.spec_list_color[index].item
+        });
+        that.price();
+    },
+    //选择规格
+    chioceGuige(e){
+        var that = this;
+        var index = e.currentTarget.dataset.index;
+        var id = e.currentTarget.dataset.item_id;
+        that.setData({
+            color_label_active2: index,
+            chioceGuigeText:id,
+            zuhePrice: that.data.chioceColorText + "_" + id,
+            dialogTopText2: that.data.spec_list_chima[index].item
+        });
+        that.price();
+    },
+    //价格计算
+    price(){
+        var that = this;
+        for (var i in that.data.spec_price_list){
+            if (that.data.zuhePrice == i ){
+                that.setData({
+                    dialog_price: that.data.spec_price_list[i].price
+                })
+            }
+        }
     },
     swiperchange(e) {
         this.setData({
@@ -56,16 +108,6 @@ Page({
     },
     onShow() {
         this.getDetail(this.data.id)
-    },
-    addCart(e) {
-        const goods = this.data.goods.item._id
-        App.HttpService.addCartByUser(goods)
-        .then(data => {
-            console.log(data)
-            if (data.meta.code == 0) {
-                this.showToast(data.meta.message)
-            }
-        })
     },
 
     showToast(message) {
@@ -113,8 +155,29 @@ Page({
             })
           }
           if (res.data.code == 0) {
+              console.log(res.data.data)
             that.setData({
               goods: res.data.data
+            })
+            //设置颜色
+            that.setData({
+                spec_list_color: res.data.data.spec_list.颜色
+            })
+            //这是尺码
+            that.setData({
+                spec_list_chima: res.data.data.spec_list.尺码
+            })
+            //设置价格
+            that.setData({
+                spec_price_list: res.data.data.spec_price_list
+            })
+            //设置dialog img
+            that.setData({
+                dialog_image: res.data.data.spec_list.颜色[0].src
+            })
+            //设置dialog price
+            that.setData({
+                dialog_price: res.data.data.price
             })
             if (res.data.data.spec_list.length == 0) {
                 //设置规格数据空提示
@@ -154,5 +217,15 @@ Page({
         this.setData({
             showDialog: !this.data.showDialog
         });
+    },
+    //购买数量选择
+    handleZanQuantityChange(e) {
+        var componentId = e.componentId;
+        var quantity = e.quantity;
+
+        this.setData({
+            [`${componentId}.quantity`]: quantity
+        });
+     
     }
-})
+}))

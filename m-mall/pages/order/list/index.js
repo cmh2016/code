9997168341f98ed726d9549d3684098b5229d8dd
cmh2016 <15675523332@163.com>
2017-09-19 18:22:21 +0800

    物流信息

--- m-mall/pages/order/list/index.js
+++ m-mall/pages/order/list/index.js
@@ -260,6 +260,7 @@ Page({
       }
     })
   },
+  //订单确认
   orderOk(e){
     var order_id = e.currentTarget.dataset.order_id;
     var uid = wx.getStorageSync('uid');
@@ -316,6 +317,57 @@ Page({
           },1000)
           
 
+        } else if (res.data.code == '-1') {
+          App.error(res.data.msg)
+          App.errGoLogin(res.data.data)
+        }
+      }
+    })
+  },
+  //物流信息
+  getWiLiu(e){
+    var order_id = e.currentTarget.dataset.order_id;
+    var uid = wx.getStorageSync('uid');
+    var token = wx.getStorageSync('token');
+    var that = this;
+    if (wx.showLoading) {
+      wx.showLoading({
+        title: "加载中",
+        mask: true
+      })
+    } else {
+      // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+    wx.request({
+      url: App.api + '/order/info', //仅为示例，并非真实的接口地址
+      data: {
+        uid: uid,
+        token: token,
+        order_id: order_id
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        //关闭loading
+        if (wx.hideLoading) {
+          wx.hideLoading()
+        } else {
+          // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+          wx.showModal({
+            title: '提示',
+            content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+          })
+        }
+        if (res.data.code == '0') {
+            wx.navigateTo({
+              url: '/pages/order/wuliu/index?send_number=' + res.data.data.send_number + '&send_code=' + res.data.data.send_code
+            })
         } else if (res.data.code == '-1') {
           App.error(res.data.msg)
           App.errGoLogin(res.data.data)
--- m-mall/pages/order/wuliu/index.js
+++ m-mall/pages/order/wuliu/index.js
@@ -1,4 +1,5 @@
 // pages/order/wuliu/index.js
+const App = getApp()
 Page({
 
   /**
@@ -31,9 +32,56 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getInfo(options.send_number, options.send_code)
   },
-
+  getInfo(send_number,send_code){
+    var uid = wx.getStorageSync('uid');
+    var token = wx.getStorageSync('token');
+    var that = this;
+    if (wx.showLoading) {
+      wx.showLoading({
+        title: "加载中",
+        mask: true
+      })
+    } else {
+      // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+    wx.request({
+      url: App.api + '/order/express', //仅为示例，并非真实的接口地址
+      data: {
+        uid: uid,
+        token: token,
+        send_number: send_number,
+        send_code: send_code
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        //关闭loading
+        if (wx.hideLoading) {
+          wx.hideLoading()
+        } else {
+          // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+          wx.showModal({
+            title: '提示',
+            content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+          })
+        }
+        if (res.data.code == '0') {
+          console.log(res.data)
+        } else if (res.data.code == '-1') {
+          App.error(res.data.msg)
+          App.errGoLogin(res.data.data)
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
